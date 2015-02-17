e776f0bd2367ed97c1b87c815f8e1c1b8e485653
Kenneth To <kennethkto@gmail.com>
2015-02-17 12:58:25 -0700

    Redirect non logged in users from clubs and students pages

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,11 +1,19 @@
 class ClubsController < ApplicationController
 
+  before_action :authenticate
+
   def index
     @clubs = Club.all
   end
 
   def show
     @club = Club.find(params[:id])
+  end
+
+  def authenticate
+    if not current_student
+    redirect_to login_path, notice: 'Not logged in'
+    end
   end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
 
+  before_action :authenticate
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def authenticate
+    if not current_student
+    redirect_to login_path, notice: 'Not logged in'
+    end
   end
 
 end
